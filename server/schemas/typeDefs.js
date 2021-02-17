c50e62675ac4f7e6ea4102fbb173e50169f4d2f9
Ricky Bobby <richard.pajewski@gmail.com>
2021-02-17 15:10:32 -0600

    typeDefs finished for now

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,56 @@
 const { gql } = require('apollo-server-express')
 
 const typeDefs = gql`
+    type RequestOff {
+        _id: ID
+        timeOff: String
+        reason: String
+        paidTimeOff: Boolean
+        approved: Boolean
+        createdAt: Date
+        approvedOn: Date
+    }
 
+    type Employee {
+        _id: ID
+        firstName: String
+        lastName: String
+        phoneNumber: String
+        email: String
+        role: String
+        requestOffs: [RequestOff]
+    }
+
+    type Auth {
+        token: ID!
+        employee: Employee
+    }
+
+    type Query {
+        me: Employee
+        employees: [Employee]
+        requestOffs: [RequestOff]
+        requests(employee: ID!): [RequestOff]
+    }
+
+    input EmployeeInput {
+        firstName: String!
+        lastName: String!
+        phoneNumber: String!
+        email: String!
+        password: String!
+        role: String!
+    }
+
+    type Mutation {
+        addUser(newEmployee: EmployeeInput!): Auth
+        login(email String!, password: String!): Auth
+        updateEmployee(phoneNumber: String!, email: String!, password: String!): Employee
+        addRequestOff(employee: ID!): Employee
+        updateRequestOff(_id: ID!, timeOff: String!, paidTimeOff: Boolean!): Employee
+        approveRequestOff(_id: ID!, approvedOn: Boolean!): Employee
+        deleteRequestOff(requestOffId: ID!): Employee
+    }
 `
 
 module.exports = typeDefs
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -22,7 +22,7 @@ module.exports = {
 
         try {
             const { data } = jwt.verify(token, secret, { maxAge: expiration })
-            req.user = data
+            req.employee = data
         }
         catch {
             console.log('Invalid Token')
