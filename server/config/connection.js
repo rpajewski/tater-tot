abbe0606d0ded46798e4c044d6efdfca515781fd
Ricky Bobby <richard.pajewski@gmail.com>
2021-02-28 15:16:27 -0600

    tweaks to name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tator-tot', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tater-tot', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
